5c8fb4ef7c7e9c2bd9f803066f7dcbaaf0495da5
hamdi <pophamdi@gmail.com>
2018-10-19 11:50:08 +0100

    initial commit

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamdi
+ * Date: 17/10/18
+ * Time: 20:41
+ */
+namespace Album;
+
+use Zend\Router\Http\Segment;
+
+
+return [
+
+
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
